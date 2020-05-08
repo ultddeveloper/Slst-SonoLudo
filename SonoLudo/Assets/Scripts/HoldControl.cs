981d8e7bd36db4bf0376ca3d97f7b08e0bef8fb7
ak <andrew.mutien@gmail.com>
2020-05-09 00:14:01 +0800

    Updating Game

---
+++ SonoLudo/Assets/Scripts/HoldControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldControl : MonoBehaviour
+{
+    public GlobalData gd;
+    int length;
+
+    bool inexit;
+
+    void Init(int len)
+    {
+        length = len;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -gd.speed, 0);
+        if (this.transform.position.y < gd.location.w && !inexit)
+        { //Miss
+
+            gd.NoteDone(3);
+            exit();
+        }
+    }
+
+
+    public void tapped()
+    {
+        float y = this.transform.position.y;
+        if (y < gd.location.z)
+        { //Perfect
+            Debug.Log("Perfect");
+            gd.NoteDone(0);
+            exit();
+        }
+        else if (y < gd.location.y)
+        { //Good
+            gd.NoteDone(1);
+            exit();
+        }
+        else if (y < gd.location.x)
+        { //Bad
+            gd.NoteDone(2);
+            exit();
+        }
+
+
+    }
+
+    public void exit()
+    {
+        inexit = true;
+        this.GetComponent<BoxCollider>().enabled = false;
+        Destroy(gameObject);
+    }
+}
---
+++ SonoLudo/Assets/Scripts/TapControl.cs
@@ -0,0 +1,59 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapControl : MonoBehaviour
+{
+    public GlobalData gd;
+
+    bool inexit;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -gd.speed, 0);
+        if (this.transform.position.y < gd.location.w && !inexit)
+        { //Miss
+            
+            gd.NoteDone(3);
+            exit();
+        }
+    }
+
+
+    public void tapped()
+    {
+        float y = this.transform.position.y;
+        if (y < gd.location.z)
+        { //Perfect
+            Debug.Log("Perfect");
+            gd.NoteDone(0);
+            exit();
+        }
+        else if (y < gd.location.y)
+        { //Good
+            gd.NoteDone(1);
+            exit();
+        }
+        else if (y < gd.location.x)
+        { //Bad
+            gd.NoteDone(2);
+            exit();
+        }
+
+        
+    }
+
+    public void exit()
+    {
+        inexit = true;
+        this.GetComponent<BoxCollider>().enabled = false;
+        Destroy(gameObject);
+    }
+}
